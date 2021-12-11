9bf59b0878aa1debc0f44daaed01b4edab488315
Augustin C Benny <84260365+augustincbenny@users.noreply.github.com>
2021-12-11 13:03:37 +0530

    Create matrixsum.c

---
+++ matrixsum.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int a[100][100],b[100][100],s[100][100],m,n,i,j;
+	printf("enter the no of rows:");
+	scanf("%d",&m);
+	printf("enter the no of columns:");
+	scanf("%d",&n);
+    printf("enter the elements for the first matrix\n");
+    for(i=0;i<m;i++)
+    {
+		for(j=0;j<n;j++)
+		scanf("%d",&a[i][j]);
+	}
+	printf("enter the elements for the second matrix\n");
+    for(i=0;i<m;i++)
+    {
+		for(j=0;j<n;j++)
+		scanf("%d",&b[i][j]);
+	}
+	printf("the sum=\n");
+	for(i=0;i<m;i++)
+    {
+		for(j=0;j<n;j++)
+		{
+			s[i][j]=a[i][j]+b[i][j];
+			printf("%3d",s[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+	
