acb2aedb6290df67566ab5ec9f8b74f5eca4bd64
Augustin C Benny <84260365+augustincbenny@users.noreply.github.com>
2021-12-11 15:29:51 +0530

    floyds triangle

---
+++ floydstriangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int n,i,j,k = 1;
+
+   n = 10;
+
+   for(i = 1; i <= n; i++) {
+      for(j = 1;j <= i; j++)
+         printf("%3d", k++);
+
+      printf("\n");
+   }
+
+   return 0;
+}
